1d5f018cdab818fb0d5568eb843d3a0f2e50acee
Ronald <ronystivens@hotmail.com>
2020-09-04 00:38:26 +0200

    added function to render bindPopup to municipalities registered

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@
 /* tile from: https://wiki.openstreetmap.org/wiki/Tiles  */
 
 
-const map = L.map('map-template').setView([40.4167, -3.70325], 11);
+const map = L.map('map-template').setView([40.4167, -3.70325], 10);
 //L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png').addTo(map);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm9ueXN0aXZlbnMiLCJhIjoiY2s5anI1dzhjMDlzOTNlcGl1NTdxeHN1biJ9.xKlCOyq7HpR2PcMgPA0Isw', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -13,31 +13,41 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
             accessToken: 'your.mapbox.access.token'
         }).addTo(map);
 
-
-function addPopUp(x){
-    L.marker(x).addTo(map);
+//funcion para agregar los marcadores
+function addPopUp(x,y){
+    L.marker(x).bindPopup(`${y}`).addTo(map);
 }
 
+//funcion para encontrar los municipios registrados en BBDD y emparejarlos con sus coordenadas:
 function coordenadas(){
     const coordenadas = {
+        "Alcalá de Henares": [40.48205, -3.35996],
+        "Alcobendas": [40.54746, -3.64197],
         "Arroyomolinos" : [40.26951, -3.91946],
         "Madrid" :[40.4165, -3.70256],
         "Rivas-Vaciamadrid" : [40.32605, -3.51089]
-    }
-    let x = coordenadas.Arroyomolinos;
-    //console.log("se va a agregar:",x) 
-    addPopUp(x);
 
-    const value = []
+    }
+   
+    //municipios registrados en la base de datos:
+    var values = []
     const longitud = document.getElementsByClassName("municipality").length
     for (i=0; i<longitud; i++){
         const muni = document.getElementsByClassName("municipality")[i].innerHTML;
-            console.log(muni)
-    
+            values.push(muni);
     }
     
+    //coincidencia con la lista de todos los municipios
+    for(y in coordenadas){
+        let x = coordenadas[y];
+
+        if(values.includes(y)){
+            addPopUp(x,y); //le agrego el marker
+        }
+    }
+
 }
-setTimeout(popup,3000);
+setTimeout(coordenadas,3000); //para que le de tiempo a renderizar los municipios
 
 
 
